7ae08c6f012474cf74d5c4b070ca9674dfcc2186
Jonam Adhikari <jadhikari131@gmail.com>
2020-07-04 19:21:19 +0900

    refs test making nev bar and adding some image

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'welcome#index' , as: 'home'
+  get 'about' => 'welcome#about', as: 'about'
+  get 'contact' => 'welcome#contact', as: 'contact'
+  get 'service' => 'welcome#service', as: 'service'
+  get 'loan' => 'welcome#loan', as: 'loan'
+  get 'room' => 'welcome#room', as: 'room'
+  get 'hostel' => 'welcome#hostel', as: 'hostel'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
