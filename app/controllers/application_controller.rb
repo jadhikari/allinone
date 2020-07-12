1c30218c30780ca9c21eae90b476c8ba87788c94
Jonam Adhikari <jadhikari131@gmail.com>
2020-07-12 16:17:12 +0900

    refs test adding some of the picture and page of the menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
+    
+    AVAILABLE_LOCALES = %w(en ja)
+    def set_locale
+        I18n.locale = params[:locale].presence || I18n.default_locale
+    end
+
+    def default_url_options(options = {})
+        {locale: I18n.locale}
+    end
 end
